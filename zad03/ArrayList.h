afa1649fe5947a078f6ae936199fc0a2cbadf1c6
rogowska <rogowska.oliwia@gmail.com>
2022-10-30 18:59:23 +0100

    h file update

--- zad03/ArrayList.h
+++ zad03/ArrayList.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <cassert>
 
+using namespace std;
+
 template <typename T>
 class ArrayList
 {
@@ -18,12 +20,37 @@ public:
         tab = new T[s];
         assert(tab != nullptr);
     } // default constructor
+
     ~ArrayList() { delete[] tab; }
 
-    ArrayList(const ArrayList &other); // copy constructor
+    ArrayList(const ArrayList &other)
+    {
+        last = other.last;
+        msize = other.msize;
+        tab = new T(msize);
+
+        for (int i = 0; i < msize; i++)
+        {
+            tab[i] = other.tab[i];
+        }
+    }; // copy constructor
     // usage:   ArrayList<int> list2(list1);
 
-    ArrayList &operator=(ArrayList &&other); // move assignment operator, return *this
+    ArrayList &operator=(ArrayList &&other)
+    {
+        delete tab;
+
+        last = other.last;
+        msize = other.msize;
+        tab = other.tab;
+
+        other.last = 0;
+        other.msize = 0;
+        other.tab = nullptr;
+
+        return *this;
+
+    }; // move assignment operator, return *this
     // usage:   list2 = std::move(list1);
 
     bool empty() const { return last == 0; }; // checks if the container has no elements
@@ -38,36 +65,40 @@ public:
     {
         if (full() == true)
         {
-            stdout("ArrayList is full, unable to add element to front");
-            return NULL;
+            cout << "ArrayList is full, unable to add element to front" << endl;
         }
-        for (auto x : tab)
+        else
         {
-            &(x + 1) = &x;
+            for (auto x : tab)
+            {
+                &(x + 1) = &x;
+            }
+            tab[0] = item;
         }
-        tab[0] = item;
     }; // dodanie na poczatek
 
     void push_back(const T &item)
     {
         if (full() == true)
         {
-            stdout("ArrayList is full, unable to add element to back");
-            return NULL;
+            cout << "ArrayList is full, unable to add element to back" << endl;
         }
-        tab[last + 1] = item;
+        else
+        {
+            tab[last + 1] = item;
+        }
     }; // dodanie na koniec
 
     T &front()
     {
         if (empty() == true)
         {
-            stdout("ArrayList is empty");
+            cout << "ArrayList is empty" << endl;
             return NULL;
         }
         else
         {
-            return tab[0];
+            return &tab[0];
         }
     }; // zwraca poczatek, nie usuwa, error dla pustej listy
 
@@ -75,12 +106,12 @@ public:
     {
         if (empty() == true)
         {
-            stdout("ArrayList is empty");
+            cout << "ArrayList is empty" << endl;
             return NULL;
         }
         else
         {
-            return tab[last];
+            return &tab[last];
         }
     }; // zwraca koniec, nie usuwa, error dla pustej listy
 
@@ -88,8 +119,7 @@ public:
     {
         if (empty() == true)
         {
-            stdout("ArrayList is empty");
-            return NULL;
+            cout << "ArrayList is empty" << endl;
         }
         else
         {
@@ -103,10 +133,9 @@ public:
 
     void pop_back()
     {
-        if (empty() == true)
+        if (empty())
         {
-            stdout("ArrayList is empty");
-            return NULL;
+            cout << "ArrayList is empty" << endl;
         }
         else
         {
@@ -118,21 +147,91 @@ public:
     void clear()
     {
         last = 0;
-        // albo
-        for (auto x : tab)
-        {
-            x = NULL;
-        }
 
     }; // czyszczenie listy z elementow
 
-    void display(){}; // lepiej zdefiniowac operator<<
+    void display()
+    {
+        cout << endl
+             << "[";
+        for (int i = 0; i < last + 1; i++)
+        {
+            cout << this[i] << " ";
+        }
+        cout << "]" << endl;
+    }; // lepiej zdefiniowac operator<<
 
-    void reverse(){}; // odwracanie kolejnosci
+    void reverse()
+    {
+        ArrayList<T> reversedArray = new ArrayList(msize);
+        while (!empty())
+        {
+            reversedArray.push_back[*this.front()];
+            this.pop_back();
+        }
+        this = reversedArray;
+        delete reversedArray;
+    }; // odwracanie kolejnosci
 
-    void sort(){}; // sortowanie listy
+    void sort()
+    {
+        if (empty())
+        {
+            cout << "Array list is empty!" << endl;
+            return;
+        }
+        else
+        {
+            int i;
+            int min;
 
-    void merge(ArrayList &other){}; //  merges two sorted lists into one
+            for (i = 0; i < last; i++)
+            {
+                min = i;
+
+                for (int j = i + 1; j < last; j++)
+                    if (tab[min] > tab[j])
+                        min = j;
+                swap(tab[min], tab[i]);
+            }
+        }
+    }; // sortowanie listy
+
+    void merge(ArrayList &other)
+    {
+        int newSize = this.size() + other.size();
+        ArrayList<T> mergedArray = new ArrayList(newSize);
+        while (!this->empty() && !other.empty())
+        {
+            if (this->front() < other.front())
+            {
+                mergedArray.push_front(this->front());
+                this->pop_front();
+            }
+            else
+            {
+                mergedArray.push_front(other.front());
+                other.pop_front();
+            }
+        }
+        if (this->empty())
+        {
+            while (!other.empty())
+            {
+                mergedArray.push_front(other.front());
+                other.pop_front();
+            }
+        }
+        else
+        {
+            while (!this->empty())
+            {
+                mergedArray.push_front(this->front());
+                this->pop_front();
+            }
+        }
+
+    }; //  merges two sorted lists into one
 };
 
 #endif
---
+++ zad03/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "ArrayList.h"
+using namespace std;
+
+int main(){
+    ArrayList<int> lista(10);
+    lista.display();
+    return 0;
+}
